11fd4d720d17b4ce98092539d6fc94a6243e258c
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-10-02 08:42:50 -0500

    Update model-teams.php

--- model-teams.php
+++ model-teams.php
@@ -1,1 +1,15 @@
-
+<?php
+function selectTeam() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT team_id, team_name, team_number FROM team");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
