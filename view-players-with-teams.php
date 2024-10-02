e2a1ed340efd5e9a3c9dc8e77b6db7820ea82cd8
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-10-02 11:00:31 -0500

    Update view-players-with-teams.php

--- view-players-with-teams.php
+++ view-players-with-teams.php
@@ -7,13 +7,16 @@ while ($player = $players->fetch_assoc()) {
     <div class="card-body">
       <h5 class="card-title"><?php echo $player['player_name']; ?></h5>
       <p class="card-text">
+      <ul class="list-group">
   <?php
     $teams = selectTeamsByPlayer($player['player_id']);
     while ($team = $teams->fetch_assoc()) {
   ?>
+      <li class="list-group-item"><?php echo $player['team_name']; ?> - <?php echo $player['season']; ?> - <?php echo $player['location']; ?> - <?php echo $player['daytime']; ?></li>
   <?php
     }
   ?>
+      </ul>
       </p>
       <p class="card-text"><small class="text-body-secondary">Number: <?php echo $player['player_number']; ?></small></p>
     </div>
