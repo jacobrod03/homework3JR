89fd6ec9f3f174c9a1d95dec16b6dc233cbfeb99
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-10-01 22:26:03 -0500

    Create model-players.php

---
+++ model-players.php
@@ -0,0 +1,15 @@
+<?php
+function selectPlayers() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("select player_id, player_name, player_number FROM player");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
