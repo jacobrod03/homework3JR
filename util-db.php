fc212f9cc5910228605e99e6cf5c95f186bbf704
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-10-01 19:24:17 -0500

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -19,3 +19,16 @@ if ($conn->connect_error) {
 }
 
 ?>
+
+<?php
+function get_db_connection(){
+    // Create connection
+    $conn = new mysqli('68.97.240.12', 'HW3JR', $password, 'hw3mis4013'); //could use local host instead of IP?
+    
+    // Check connection
+    if ($conn->connect_error) {
+      return false;
+    }
+    return $conn;
+}
+?>
