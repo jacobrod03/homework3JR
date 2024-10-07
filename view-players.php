84bc6f42b9b3d1516ecf8e6e77b4d26acbac1998
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-10-07 13:29:52 -0500

    Update view-players.php

--- view-players.php
+++ view-players.php
@@ -17,7 +17,7 @@ while ($player = $players->fetch_assoc()) {
     <td><?php echo $player['player_id']; ?></td>
     <td><?php echo $player['player_name']; ?></td>
     <td><?php echo $player['player_number']; ?></td>
-    <td><a href="teams-by-player.php?id=<?php echo $player['player_id']; ?>">Teams</a></td>
+    <td><a href="teams-by-players.php?id=<?php echo $player['player_id']; ?>">Teams</a></td>
   </tr>
   <?php
 }
