513c336865c8e9d978f78cfaaa761aea2c09b232
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-10-07 11:49:04 -0500

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,11 +1,18 @@
 <?php
 function get_db_connection(){
     // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname, $port); //could use local host instead of IP?
+    $servername = "hw3mis4013.mysql.database.azure.com";
+    $username = "hw3jjr";
+    $password = "Database3909";
+    $dbname = "hw3jjr";
+    $port = 3306;
+    
+    
+$conn = new mysqli($servername, $username, $password, $dbname, $port); //could use local host instead of IP?
     
     // Check connection
     if ($conn->connect_error) {
-      return false;
+      die("Connection failed: " . $conn->connect_error);
     }
     return $conn;
 }
