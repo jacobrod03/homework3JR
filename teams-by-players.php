e1ff30eb66a25a96af07506da7e5e1ab3c7fb154
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-10-07 14:45:30 -0500

    Update teams-by-players.php

--- teams-by-players.php
+++ teams-by-players.php
@@ -4,7 +4,7 @@ require_once("model-teams-by-player.php");
 
 $pageTitle = "Teams by Player";
 include "view-header.php";
-$team = selectTeambyPlayer($_GET['id']);
+$team = selectTeamsbyPlayer($_GET['id']);
 include "view-team-by-player.php";
 include "view-footer.php";
 ?>
