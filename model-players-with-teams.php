7b92d60e0fa1819be785f59ddccbfa2a77b6c28c
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-10-07 23:12:24 -0500

    Update model-players-with-teams.php

--- model-players-with-teams.php
+++ model-players-with-teams.php
@@ -14,11 +14,11 @@ function selectPlayers() {
 }
 ?>
 <?php
-function selectTeamsByPlayer($tid) {
+function selectTeamsByPlayer($pid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT t.team_id, team_name, team_number, season, location, daytime FROM team t join league l on l.team_id = t.team_id where l.player_id = ?");
-        $stmt->bind_param("i", $tid); 
+        $stmt->bind_param("i", $pid); 
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
