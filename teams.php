27a3e4d4b902a4109e1ac818720d6ef8ed6327ac
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-10-07 21:35:26 -0500

    Update teams.php

--- teams.php
+++ teams.php
@@ -2,7 +2,7 @@
 require_once("util-db.php");
 require_once("model-teams.php");
 
-$pageTitle = "Players";
+$pageTitle = "Teams";
 include "view-header.php";
 $team = selectTeam();
 include "view-players.php";
