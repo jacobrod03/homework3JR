1cf05273ab87be1dc57c5fe821870fd6b1be3bb9
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-10-07 22:36:41 -0500

    Update players-with-teams.php

--- players-with-teams.php
+++ players-with-teams.php
@@ -4,7 +4,7 @@ require_once("model-players-with-teams.php");
 
 $pageTitle = "Players with Teams";
 include "view-header.php";
-$player = selectPlayers();
+$players = selectPlayers();
 include "view-players-with-teams.php";
 include "view-footer.php";
 ?>
