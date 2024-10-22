1131cec8a5dd31b163649d015ffa525e55d80655
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-10-21 22:23:27 -0500

    Update model-teams.php

--- model-teams.php
+++ model-teams.php
@@ -12,4 +12,46 @@ function selectTeam() {
         throw $e;
     }
 }
+
+function insertTeam($tName, $tNumber) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `hw3`.`team` (`team_name`, `team_number`) VALUES (?, ?)");
+        $stmt->bind_param("ss", $tName, $tNumber);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateTeam($tName, $tNumber, $tid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `hw3`.`team` set `team_name` = ?, `team_number` = ? where team_id = ?");
+        $stmt->bind_param("ssi", $tName, $tNumber, $tid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteTeam($tid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from team where team_id=?");
+        $stmt->bind_param("i", $tid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
