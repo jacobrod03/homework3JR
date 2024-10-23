38103191a6bd5f79e8866c05bdb51157e8429a6f
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-10-23 11:38:22 -0500

    Update model-players-with-teams.php

--- model-players-with-teams.php
+++ model-players-with-teams.php
@@ -28,6 +28,20 @@ function selectTeamsByPlayer($pid) {
         throw $e;
     }
 }
+
+function selectPlayersForInput() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("select player_id, player_name, player_number FROM player");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 //might not need?
 function insertLeagues($pid, $tid, $season, $location, $daytime) {
     try {
