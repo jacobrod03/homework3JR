a8247634822913588589cf2ab3f2ea6983f90467
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-10-22 19:48:08 -0500

    Update teams.php

--- teams.php
+++ teams.php
@@ -4,6 +4,15 @@ require_once("model-teams.php");
 
 $pageTitle = "Teams";
 include "view-header.php";
+
+if isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+  case "Add":
+    insertTeam($_POST['tName'], $_POST['tNumber']);
+    break;
+  }
+}
+  
 $team = selectTeam();
 include "view-teams.php";
 include "view-footer.php";
