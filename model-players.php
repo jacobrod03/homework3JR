52dd0d4846a719c28cf1c8df91b7ba1a543cf4ef
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-10-28 18:52:49 -0500

    Update model-players.php

--- model-players.php
+++ model-players.php
@@ -13,14 +13,42 @@ function selectPlayers() {
     }
 }
 
-function insertPlayer() {
+function insertPlayer($pName, $pNumber) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("select player_id, player_name, player_number FROM player");
-        $stmt->execute();
-        $result = $stmt->get_result();
+        $stmt = $conn->prepare("INSERT INTO `hw3`.`player` (`player_name`, `player_number`) VALUES (?, ?)");
+        $stmt->bind_param("ss", $pName, $pNumber);
+        $success = $stmt->execute();
         $conn->close();
-        return $result;
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updatePlayer($pName, $pNumber, $pid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `hw3`.`player` set `player_name` = ?, `player_number`= ? where player_id =?");
+        $stmt->bind_param("ssi", $pName, $pNumber, $pid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deletePlayer($pid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from player where player_id = ?");
+        $stmt->bind_param("i", $pid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
     } catch (Exception $e) {
         $conn->close();
         throw $e;
