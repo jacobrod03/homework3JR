cef7590d54b0074fa6f1d88a462e10469d457ecb
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-10-28 13:14:17 -0500

    Update model-players.php

--- model-players.php
+++ model-players.php
@@ -12,4 +12,18 @@ function selectPlayers() {
         throw $e;
     }
 }
+
+function insertPlayer() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("select player_id, player_name, player_number FROM player");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
