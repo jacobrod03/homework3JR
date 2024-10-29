6a1255d81d5655bfd79b9c2b80911fc28732e9d3
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-10-28 19:54:41 -0500

    Update players.php

--- players.php
+++ players.php
@@ -8,7 +8,11 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      insertPlayer($_POST['pName'], $_POST['pNumber']);
+      if (insertPlayer($_POST['pName'], $_POST['pNumber'])) {
+        echo '<div class="alert alert-success" role="alert">Player added! </div>"';
+      } else {
+       echo '<div class="alert alert-danger" role="alert">Error </div>"';
+      }
       break;
   }
 }
