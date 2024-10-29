e50364dc439bce19f47058062fc0598ca3e09483
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-10-28 19:32:29 -0500

    Update players.php

--- players.php
+++ players.php
@@ -4,6 +4,15 @@ require_once("model-players.php");
 
 $pageTitle = "Players";
 include "view-header.php";
+
+if isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add";
+      insertPlayer($_POST['cName'], $_POST['cNumber']);
+      break;
+  }
+}
+  
 $players = selectPlayers();
 include "view-players.php";
 include "view-footer.php";
