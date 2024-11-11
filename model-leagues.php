00db7e26f3c6cd6a136cf9616ae6153071a59932
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-11-11 14:22:12 -0600

    Update model-leagues.php

--- model-leagues.php
+++ model-leagues.php
@@ -2,9 +2,7 @@
 function selectLeagues() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("select a.account_id, account_name, o.active, ifnull((select count(account_user_id) from account_user where account_id=a.account_id),0) as users 
-            from account a join account_owner o on o.account_id=a.account_id where owner_email=? order by account_name");
-        $stmt->bind_param("s", $uEmail);
+        $stmt = $conn->prepare("SELECT league_id, season, location FROM league");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
