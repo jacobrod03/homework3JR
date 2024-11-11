26cc9521bd6132f1f51f1f8ee9dbf53067c97674
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-11-11 14:59:35 -0600

    Create leagues-by-player.php

---
+++ leagues-by-player.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-leagues-by-player.php");
+
+$pageTitle = "Leagues by Player";
+include "view-header.php";
+$leagues = selectLeaguesbyPlayer($_GET['id']);
+include "view-leagues-by-player.php";
+include "view-footer.php";
+?>
