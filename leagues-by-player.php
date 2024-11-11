073e75a9d7ee97726e3eef40d4a90dc388efc95e
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-11-11 16:38:00 -0600

    Update leagues-by-player.php

--- leagues-by-player.php
+++ leagues-by-player.php
@@ -4,7 +4,7 @@ require_once("model-leagues-by-player.php");
 
 $pageTitle = "Leagues by Player";
 include "view-header.php";
-$leagues = selectLeaguesbyPlayer($_GET['id']); //change back to get
+$leagues = selectLeaguesbyPlayer($_GET['pid']); //change back to get
 include "view-leagues-by-player.php";
 include "view-footer.php";
 ?>
