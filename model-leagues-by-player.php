1cbae038170dbfcbc3d958df3def5ded985c09e8
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-11-11 22:24:02 -0600

    Update model-leagues-by-player.php

--- model-leagues-by-player.php
+++ model-leagues-by-player.php
@@ -1,9 +1,9 @@
 <?php
-function selectLeaguesbyPlayer($lid) {
+function selectLeaguesbyPlayer($pid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT l.league_id, season, location, daytime FROM league l join player p on p.player_id = l.player_id where p.player_id=?");
-        $stmt->bind_param("i", $lid);
+        $stmt->bind_param("i", $pid);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
