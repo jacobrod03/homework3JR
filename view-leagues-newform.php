9ea6f1b8d6ede30c3f72393b62d1d4183c019a75
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-11-13 16:31:47 -0600

    Update view-leagues-newform.php

--- view-leagues-newform.php
+++ view-leagues-newform.php
@@ -14,21 +14,16 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        <form>
+        <form method="post" action="">
       <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        <label for="player_id" class="form-label">Player ID</label>
+        <input type="int" class="form-control" id="player_id" name="player_id">
       </div>
-      <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
+     <div class="mb-3">
+        <label for="team_id" class="form-label">Team ID</label>
+        <input type="int" class="form-control" id="team_id" name="team_id">
       </div>
-      <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-      </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
+      <button type="submit" class="btn btn-primary">Save</button>
     </form>
       </div>
     </div>
