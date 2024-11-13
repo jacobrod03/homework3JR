bbdb2b06907852f5022e0007e36d8f5a2afd3c90
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-11-13 13:55:22 -0600

    Update model-leagues.php

--- model-leagues.php
+++ model-leagues.php
@@ -13,15 +13,42 @@ function selectLeagues() {
     }
 }
 
-function insertLeagues() {
+function insertLeagues($pid, $tid, $season, $location, $daytime) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("INSERT INTO `hw3`.`league` (`league_id`, `player_id`, `team_id`, `season`, `location`, `daytime`) VALUES ('6', '5', '6', 'spring 2000', 'Atlanta, GA', '9 PM');
-");
-        $stmt->execute();
-        $result = $stmt->get_result();
+        $stmt = $conn->prepare("INSERT INTO `hw3`.`league` (`player_id`, `team_id`, `season`, `location`, `daytime`) VALUES (?, ?, ?, ?, ?)");
+        $stmt->bind_param("iisss", $pid, $tid, $season, $location, $daytime);
+        $success = $stmt->execute();
         $conn->close();
-        return $result;
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateLeagues($pid, $tid, $season, $location, $daytime, $lid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `hw3`.`league` set `player_id` = ?, `team_id` = ?, `season` = ?, `location` = ?, `daytime` = ? where league_id = ?");
+        $stmt->bind_param("iisssi", $pid, $tid, $season, $location, $daytime, $lid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteLeagues($lid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from league where league_id=?");
+        $stmt->bind_param("i", $lid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
     } catch (Exception $e) {
         $conn->close();
         throw $e;
