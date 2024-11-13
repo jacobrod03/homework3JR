7e2d17dcd1944740555474c7fae576cae1fb13a1
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-11-13 13:34:56 -0600

    Update model-leagues.php

--- model-leagues.php
+++ model-leagues.php
@@ -12,4 +12,19 @@ function selectLeagues() {
         throw $e;
     }
 }
+
+function insertLeagues() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `hw3`.`league` (`league_id`, `player_id`, `team_id`, `season`, `location`, `daytime`) VALUES ('6', '5', '6', 'spring 2000', 'Atlanta, GA', '9 PM');
+");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
