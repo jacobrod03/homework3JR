a3be6594d25cf6ab262d6ef9345018ac8cfed787
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-11-13 16:50:12 -0600

    Update leagues.php

--- leagues.php
+++ leagues.php
@@ -4,6 +4,15 @@ require_once("model-leagues.php");
 
 $pageTitle = "Leagues";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch($_POST['actionType']) {
+    case "Add":
+    insertLeagues($_POST['season'], $_POST['location']);
+    break;
+  }
+}
+
 $leagues = selectLeagues();
 include "view-leagues.php";
 include "view-footer.php";
