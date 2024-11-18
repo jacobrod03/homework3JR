6419eb6c1e0151c9c4282b7736d666fcd63f62cd
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-11-18 13:50:00 -0600

    Update leagues.php

--- leagues.php
+++ leagues.php
@@ -9,9 +9,6 @@ if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
     if (insertLeagues($_POST['Sseason'], $_POST['Llocation']);
-    echo <div class="alert alert-success" role="alert">Item Added</div>
-      } else {
-  <div class= "alert alert-danger" role="alert"> Error in adding </div>
     break;
   }
 }
