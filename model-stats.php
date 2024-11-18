d03dbf7db369fd5eb2e4179caa0fc46b686b88c7
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-11-18 17:37:34 -0600

    Update model-stats.php

--- model-stats.php
+++ model-stats.php
@@ -12,4 +12,18 @@ function selectStats() {
         throw $e;
     }
 }
+
+function insertStats() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare(INSERT INTO `hw3`.`stats` (`stats_hits`, `stats_errors`) VALUES ('5', '6'););
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
