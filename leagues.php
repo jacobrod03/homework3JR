dcf6e1e8b091937d5a913ec1c5a0b161aaf69d40
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-11-18 15:25:37 -0600

    Update leagues.php

--- leagues.php
+++ leagues.php
@@ -14,6 +14,13 @@ if (isset($_POST['actionType'])) {
       echo '<div class="alert alert-danger" role="alert"> Error. </div>';
     }
     break;
+    case "Edit":
+    if (updateLeagues($_POST['Sseason'], $_POST['Llocation'], $_POST['lid'])) {
+      echo '<div class="alert alert-success" role="alert"> League edited!</div>';
+    } else {
+      echo '<div class="alert alert-danger" role="alert"> Error. </div>';
+    }
+    break;
     case "Delete":
     if (deleteLeagues($_POST['lid'])) {
       echo '<div class="alert alert-success" role="alert"> League deleted. </div>';
