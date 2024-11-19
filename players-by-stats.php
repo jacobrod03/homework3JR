432de14262e4c593e7d342b0bc06b2db6dccb061
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-11-18 18:06:26 -0600

    Create players-by-stats.php

---
+++ players-by-stats.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-players-by-stats.php");
+
+$pageTitle = "Players by stats";
+include "view-header.php";
+$stats = selectPlayersByStats($_GET['id']);
+include "view-players-by-stats.php";
+include "view-footer.php";
+?>
