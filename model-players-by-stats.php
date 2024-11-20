ea90baca3e9952891f28965938c8ee0db165126a
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-11-20 14:27:32 -0600

    Update model-players-by-stats.php

--- model-players-by-stats.php
+++ model-players-by-stats.php
@@ -4,7 +4,7 @@ function selectPlayersByStats($sid) {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT s.stats_id, stats_hits, stats_errors, location FROM stats s join league l on l.stats_id = s.stats_id WHERE l.player_id = ?");
         $stmt->execute();
-        $stmt->bind_param("s", $sid);
+        $stmt->bind_param("i", $sid);
         $result = $stmt->get_result();
         $conn->close();
         return $result;
