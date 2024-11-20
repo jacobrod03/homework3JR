0828c42aa1fbeb91ecbc8efc250e0a563fbe7be3
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-11-20 17:56:57 -0600

    Update view-stats-newform.php

--- view-stats-newform.php
+++ view-stats-newform.php
@@ -24,6 +24,7 @@
               <label for="sErrors" class="form-label">Stats errors</label>
               <input type="text" class="form-control" id="sErrors" name="sErrors">
             </div>
+          <input type="hidden" name="actionType" value="Add">
             <button type="submit" class="btn btn-primary">Save</button>
         </form>
       </div>
