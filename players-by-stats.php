d0c4383b9a5a3d2b71bfdc016b69def3bb6d85ce
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-11-20 14:47:58 -0600

    Update players-by-stats.php

--- players-by-stats.php
+++ players-by-stats.php
@@ -4,7 +4,7 @@ require_once("model-players-by-stats.php");
 
 $pageTitle = "Players by stats";
 include "view-header.php";
-$stats = selectPlayersByStats($_GET['id']);
+$stats = selectPlayersByStats($_POST['sid']);
 include "view-players-by-stats.php";
 include "view-footer.php";
 ?>
