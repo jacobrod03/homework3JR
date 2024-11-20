14f0363be9c0cd79f5a12ddfed974c4ecc774484
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-11-20 14:12:34 -0600

    Create view-players-by-stats.php

---
+++ view-players-by-stats.php
@@ -0,0 +1,29 @@
+<h1>Players by stats</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>ID</th>
+      <th>Hits</th>
+      <th>Errors</th>
+      <th>Location</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($stat = $stats->fetch_assoc()) {
+?>
+  <tr>
+    <td><?php echo $stat['stats_id']; ?></td>
+    <td><?php echo $stat['stats_hits']; ?></td>
+    <td><?php echo $stat['stats_errors']; ?></td>
+    <td><?php echo $stat['location']; ?></td>
+    <td><a href="players-by-stats.php?id=<?php echo $stat['stats_id']; ?>">Players</a></td>
+  </tr>
+<?php
+}
+?>
+    </tbody>
+  </table>
+</div>
