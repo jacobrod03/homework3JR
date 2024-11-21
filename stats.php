a6bdd2c2d77fa45a1e42fb0847b5f33cdbc79a2e
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-11-20 18:00:53 -0600

    Update stats.php

--- stats.php
+++ stats.php
@@ -4,6 +4,15 @@ require_once("model-stats.php");
 
 $pageTitle = "Stats";
 include "view-header.php";
+
+if (isset($_POST['actionType'])){
+  switch ($_POST['actionType']) {
+    case "Add":
+      insertStats($_POST['sHits'], $_POST['sErrors']);
+      break;
+  }
+}
+
 $stats = selectStats();
 include "view-stats.php";
 include "view-footer.php";
