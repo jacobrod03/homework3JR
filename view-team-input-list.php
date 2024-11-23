ecd8a6f5b0a72dd5bc5c8928bb856016f38d9d60
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-11-23 10:57:00 -0600

    Update view-team-input-list.php

--- view-team-input-list.php
+++ view-team-input-list.php
@@ -2,7 +2,7 @@
 <?php 
 while ($teamItem = $teamList->fetch_assoc()) {
 ?>
-  <option value="<?php echo $teamItem['tName']; ?>"><?php echo $teamItem['tName']; ?></option>
+  <option value="<?php echo $teamItem['team_id']; ?>"><?php echo $teamItem['tName']; ?></option>
 <?php 
 }
 ?>
